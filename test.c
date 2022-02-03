2c22448e7b437163e57819f947d0873f1b95bb42
Arnaud Blondel <ablondel@fu-r5-p4.s19.be>
2022-02-03 15:39:46 +0100

    allright

--- test.c
+++ test.c
@@ -7,12 +7,13 @@
 #define BREAK 2
 #define ARG 3
 
-typedef struct		s_list
+int		tabsize(char **tab)
 {
-	char			**args;
-	int				is_piped;
-	void			*next;
-}					t_list;
+	int i = 0;
+	while (tab[i])
+		i++;
+	return i;
+}
 
 size_t	ft_strlen(char *s)
 {
@@ -35,6 +36,8 @@ char	*ft_strdup(char *src)
 
 int		ft_type(char *av)
 {
+	if (!av)
+		return (-1);
 	if (strcmp(av, "|") == 0)
 		return PIPE;
 	else if (strcmp(av, ";") == 0)
@@ -42,151 +45,79 @@ int		ft_type(char *av)
 	return ARG;
 }
 
-void exec(t_list *cmd, char **env)
+void	printab(char **cmd)
 {
-	(void)env;
-    pid_t pid;
-    int j = 0;
-	int npipes = 0;
-    int *pfd;
-    while (cmd) 
+	for (size_t i = 0; cmd[i]; i++)
 	{
-		pfd = malloc(2*npipes*sizeof(int));
-    	for (int i = 0; i < npipes; i++)
-		{
-    	    if (pipe(pfd + i * 2) < 0) 
-			{
-    	        exit(EXIT_FAILURE);
-    	    }
-    	}
-        pid = fork();
-        if (pid == 0)
-		{
-            //if not last cmd
-            if (cmd->next)
-			{
-          		if (dup2(pfd[j + 1], 1) < 0)
-				{
-                    exit(EXIT_FAILURE);
-                }
-				npipes--;
-            }
-            //if not first cmd&& j != 2 * npipes
-            if (j != 0 && j != 2 * npipes)
-			{
-                if (dup2(pfd[j - 2], 0) < 0)
-				{
-                    exit(EXIT_FAILURE);
-                }
-            }
-            for(int i = 0; i < 2 * npipes; i++)
-			{
-                close(pfd[i]);
-            }
-            if (execve(*cmd->args, cmd->args, env) < 0)
-			{
-                exit(EXIT_FAILURE);
-            }
-        }
-		else if (pid < 0)
-		{
-            exit(EXIT_FAILURE);
-        }
-		cmd = cmd->next;
-        j += 2;
-    }
-    //Parent closes the pipes and wait for children
-    for(int i = 0; i < 2 * npipes; i++)
-	{
-        close(pfd[i]);
-    }
-    for(int i = 0; i < npipes + 1; i++)
-	{
-        waitpid(pid, NULL, 0);
+		printf("{%s}->", cmd[i]);
 	}
+	printf("\n\n");
 }
 
-void	print(t_list *lst)
+void	pipeline(char ***cmd, char **env)
 {
-	int i = 0;
-	while (lst)
+	int pfd[2];
+	pid_t pid;
+	while (*cmd)
 	{
-		printf("<<<[%d] PIPED\n", lst->is_piped);
-		while (lst->args[i])
+		printab(*cmd);
+		pipe(pfd);
+		pid = fork();
+		if (pid == 0)
 		{
-			printf("{%s}\n", lst->args[i++]);
+			if (*(cmd + 1))
+				dup2(pfd[1], 1);
+			close(pfd[0]);
+			close(pfd[1]);
+			execve((*cmd)[0], &(*cmd)[0], env);
 		}
-		printf("===NEXT>>>\n\n");
-		lst = lst->next;
-		i = 0;
+		else
+		{
+			dup2(pfd[0], 0);
+			waitpid(0,0,0);
+			close(pfd[1]);
+			close(pfd[0]);
+		}
+		cmd++;
 	}
-}
-
-t_list *newnode(char **data, int start, int end)
-{
-	int i = 0;
-	t_list *node = NULL;
-	node = malloc(sizeof(t_list));
-	node->args = malloc(sizeof(char*) * (end + 1));
-	while (start < end && ft_type(data[start]) != BREAK && ft_type(data[start]) != PIPE)
-	{
-		node->args[i] = ft_strdup(data[start]);
-		start += 1;
-		i++;
-	}
-	node->args[i] = 0;
-	node->next = NULL;
-	return node;
-}
-
-void	push(t_list **lst, t_list *node)
-{
-	t_list *tmp = *lst;
-	if (!(*lst))
-	{
-		node->next =  (*lst);
-		(*lst) = node;
-		return ;
-	}
-	while ((*lst)->next)
-	{
-		(*lst) = (*lst)->next;
-	}
-	(*lst)->next = node;
-	(*lst) = tmp;
-}
-
-int		tabsize(char **tab)
-{
-	int i = 0;
-	while (tab[i])
-		i++;
-	return i;
 }
 
 int		main(int ac, char **av, char **env)
 {
 	(void)ac;
+	(void)av;
 	(void)env;
-	int i = 1;
-	t_list *lst = NULL;
-	t_list *new = NULL;
-
+	int		i = 1;
+	char	***cmd = NULL;
+	cmd = (char***)malloc(sizeof(char**) * 150);
+	for (size_t i = 0; i < 150; i++)
+	{
+		cmd[i] = (char**)malloc(sizeof(char*) * 150);
+		for (size_t j = 0; j < 150; j++)
+		{
+			cmd[i][j] = (char*)malloc(sizeof(char) * 150);
+		}
+	}
+	size_t cmdi = 0;
+	size_t cmdj = 0;
 	if (ac >= 2)
 	{
-		while (av[i] && i < ac)
+		while (av[i])
 		{
-			if (ft_type(av[i - 1]) == PIPE)
-				new->is_piped = 1;
-			if (ft_type(av[i]) == ARG)
-			{
-				new = newnode(av, i, ac);
-				i += tabsize(new->args);
-				push(&lst, new);
-			}
+			cmd[cmdi][cmdj] = av[i];
+			cmdj++;
 			i++;
+			if (ft_type(av[i]) == BREAK || ft_type(av[i]) == PIPE)
+			{
+				cmd[cmdi][cmdj] = 0;
+				cmdj = 0;
+				cmdi++;
+				i++;
+			}
 		}
-		print(lst);
+		cmd[cmdi][cmdj] = NULL;
+		cmd[cmdi + 1] = NULL;
+		pipeline(cmd, env);
 	}
 	return 0;
 }
